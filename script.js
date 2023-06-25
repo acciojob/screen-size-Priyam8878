fb79022c503df31db1a16f76e5f51efaeb4a2eda
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-25 17:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 
-	// window.addEventListner("resize", () =>{
-	// 	const width = window.innerWidth;
-	// 	const height = window.innerHeight;
-	// 	const sizeInfo = document.getElementById('sizeInfo');
-	// 	sizeInfo.querySelector('h1').textContent = `Width:${width} and Height:${height} `
+	window.addEventListner("resize", () =>{
+		const width = window.innerWidth;
+		const height = window.innerHeight;
+		const sizeInfo = document.getElementById('sizeInfo');
+		sizeInfo.querySelector('h1').textContent = `Width:${width} and Height:${height} `
 
-	// })
+	})
 
 // <script>
   // function updateWindowSize() {
