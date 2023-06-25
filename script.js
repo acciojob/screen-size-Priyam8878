77fa6ecae380443e77194317bda3c451f4f15edb
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-25 17:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 //your JS code here. If required.
 
-	window.addEventListner("resize", () =>{
-		const width = window.innerWidth;
-		const height = window.innerHeight;
-		const sizeInfo = document.getElementById("sizeInfo");
-		sizeInfo.querySelector("h1").textContent = `Width:${width} and Height:${height} `
+	// window.addEventListner("resize", () =>{
+	// 	const width = window.innerWidth;
+	// 	const height = window.innerHeight;
+	// 	const sizeInfo = document.getElementById("sizeInfo");
+	// 	sizeInfo.querySelector("h1").textContent = `Width:${width} and Height:${height} `
 
-	})
+	// })
 
 // <script>
-//   function updateWindowSize() {
-//     const width = window.innerWidth;
-//     const height = window.innerHeight;
-//     const sizeInfo = document.getElementById('sizeInfo');
-//     sizeInfo.querySelector('h1').textContent = `Width: ${width} and Height: ${height}`;
-//   }
+  function updateWindowSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeInfo = document.getElementById('sizeInfo');
+    sizeInfo.querySelector('h1').textContent = `Width: ${width} and Height: ${height}`;
+  }
 
-//   updateWindowSize();
+  updateWindowSize();
 
-//   window.addEventListener('resize', updateWindowSize);
+  window.addEventListener('resize', updateWindowSize);
 // </script>
