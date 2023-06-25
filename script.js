76f4c602e199d1216f85d72f69ae83304762d09c
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-25 17:43:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 		const width = window.innerWidth;
 		const height = window.innerHeight;
 		const sizeInfo = document.getElementById("sizeInfo");
-		sizeInfo.querySelector("h1").textContent = `Width: ${width} and Height: ${height} `
+		sizeInfo.querySelector("h1").textContent = `Width:${width} and Height:${height} `
 
 	})
 
